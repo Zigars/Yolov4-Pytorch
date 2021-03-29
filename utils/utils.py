7d8d6262374996666460e18f727d52cbc59696ba
Zigars <1370170007@qq.com>
2021-03-29 11:14:40 +0800

    解码输出

--- utils/utils.py
+++ utils/utils.py
@@ -8,6 +8,161 @@ import torch.nn as nn
 import math
 import numpy as np
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+#---------------------------------------------------------------#
+#    解码输出检测框
+#---------------------------------------------------------------#
+class DecodeBox(nn.Module):
+    def __init__(self, anchors, num_classes, img_size):
+        super(DecodeBox, self).__init__()
+        self.anchors     = anchors
+        self.num_anchors = len(anchors)
+        self.num_classes = num_classes
+        self.bbox_attrs  = 5 + num_classes
+        self.img_size    = img_size
+
+    def forward(self, input):
+        #---------------------------------------------------------------#
+        #    输入的input一共有三层，shape分别是：
+        #    batch_size, 255, 13, 13
+        #    batch_size, 255, 26, 26
+        #    batch_size, 255, 52, 52
+        #---------------------------------------------------------------#
+        batch_size = input.size(0)
+        input_h    = input.size(2)
+        input_w    = input.size(3)
+
+        #---------------------------------------------------------------#
+        #    输入为416*416时
+        #    stride_h = stride_w = 32, 16, 8
+        #---------------------------------------------------------------#
+        stride_h = self.img_size[1] / input_h
+        stride_w = self.img_size[0] / input_w
+        
+        # 获得相对于特征层的anchors
+        scaled_anchors = [(anchor_w / stride_w, anchor_h / stride_h) for anchor_w, anchor_h in self.anchors]
+        
+        #---------------------------------------------------------------#
+        #    转换input的shape为
+        #    batch_size, 3, 13, 13, 85
+        #    batch_size, 3, 26, 26, 85
+        #    batch_size, 3, 52, 52, 85
+        #---------------------------------------------------------------#
+        prediction = input.view(batch_size, self.num_anchors, 
+                                self.bbox_attrs, input_h, input_w).permute(0, 1, 3, 4, 2).contiguous()
+        
+        # 先验框的中心位置的调整参数
+        x = torch.sigmoid(prediction[..., 0])
+        y = torch.sigmoid(prediction[..., 1])
+
+        # 先验框的宽高调整参数
+        w = prediction[..., 2]
+        h = prediction[..., 3]
+
+        # 获得置信度，是否有目标
+        conf = torch.sigmoid(prediction[..., 4])
+
+        # 种类置信度
+        pred_cls = torch.sigmoid(prediction[..., 5:])
+
+        FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
+        LongTensor = torch.cuda.LongTensor if x.is_cuda else torch.LongTensor
+
+        #---------------------------------------------------------------#
+        #    生成网格，先验框中心，网格左上角
+        #    torch.linspace():返回一个一维的tensor，
+        #    这个张量包含了从start到end，分成steps个线段得到的向量
+        #    https://blog.csdn.net/york1996/article/details/81671128
+        #    repeat():沿着指定的维度重复tensor
+        #---------------------------------------------------------------#
+        grid_x = torch.linspace(0, input_w - 1, input_w).repeat(input_h, 1).repeat(
+            batch_size * self.num_anchors, 1, 1).view(x.shape).type(FloatTensor)
+        grid_y = torch.linspace(0, input_h - 1, input_h).repeat(input_w, 1).t().repeat(
+            batch_size * self.num_anchors, 1, 1).view(y.shape).type(FloatTensor)
+
+        # 按照网格格式生成先验框的宽高
+        # batch_size, 3, 13, 13
+        anchor_w = FloatTensor(scaled_anchors).index_select(1, LongTensor([0]))
+        anchor_h = FloatTensor(scaled_anchors).index_select(1, LongTensor([1]))
+        anchor_w = anchor_w.repeat(batch_size, 1).repeat(1, 1, input_h * input_w).view(w.shape)
+        anchor_h = anchor_h.repeat(batch_size, 1).repeat(1, 1, input_h * input_w).view(h.shape)
+
+        #---------------------------------------------------------------#
+        #    利用预测结果对先验框进行调整
+        #    首先调整先验框的中心，从先验框中心向右下角偏移
+        #    再调整先验框的宽高
+        #---------------------------------------------------------------#
+        pred_boxes = FloatTensor(prediction[..., :4].shape)
+        pred_boxes[..., 0] = x.data + grid_x
+        pred_boxes[..., 1] = y.data + grid_y
+        pred_boxes[..., 2] = torch.exp(w.data) * anchor_w
+        pred_boxes[..., 3] = torch.exp(h.data) * anchor_h
+
+        self.show_anchor(input_h, pred_boxes, grid_x, grid_y, anchor_w, anchor_h)
+
+        # 将输出调整为相对于416*416的大小
+        _scale = torch.Tensor([stride_w, stride_h] * 2).type(FloatTensor)
+        output = torch.cat((pred_boxes.view(batch_size, -1, 4) * _scale, 
+                            conf.view(batch_size, -1, 1), pred_cls.view(batch_size, -1, self.num_classes)), -1)
+        return output.data
+
+    #---------------------------------------------------------------#
+    #    绘图显示先验框调整过程
+    #---------------------------------------------------------------#
+    def show_anchor(self, input_height, pred_boxes, grid_x, grid_y, anchor_w, anchor_h):
+        fig = plt.figure()
+        ax = fig.add_subplot(121)
+        if input_height==13:
+            plt.ylim(0,13)
+            plt.xlim(0,13)
+        elif input_height==26:
+            plt.ylim(0,26)
+            plt.xlim(0,26)
+        elif input_height==52:
+            plt.ylim(0,52)
+            plt.xlim(0,52)
+        plt.scatter(grid_x.cpu(),grid_y.cpu())
+
+        anchor_left = grid_x - anchor_w/2 
+        anchor_top = grid_y - anchor_h/2 
+
+        rect1 = plt.Rectangle([anchor_left[0,0,5,5],anchor_top[0,0,5,5]],anchor_w[0,0,5,5],anchor_h[0,0,5,5],color="r",fill=False)
+        rect2 = plt.Rectangle([anchor_left[0,1,5,5],anchor_top[0,1,5,5]],anchor_w[0,1,5,5],anchor_h[0,1,5,5],color="r",fill=False)
+        rect3 = plt.Rectangle([anchor_left[0,2,5,5],anchor_top[0,2,5,5]],anchor_w[0,2,5,5],anchor_h[0,2,5,5],color="r",fill=False)
+
+        ax.add_patch(rect1)
+        ax.add_patch(rect2)
+        ax.add_patch(rect3)
+
+        ax = fig.add_subplot(122)
+        if input_height==13:
+            plt.ylim(0,13)
+            plt.xlim(0,13)
+        elif input_height==26:
+            plt.ylim(0,26)
+            plt.xlim(0,26)
+        elif input_height==52:
+            plt.ylim(0,52)
+            plt.xlim(0,52)
+        plt.scatter(grid_x.cpu(),grid_y.cpu())
+        plt.scatter(pred_boxes[0,:,5,5,0].cpu(),pred_boxes[0,:,5,5,1].cpu(),c='r')
+
+        pre_left = pred_boxes[...,0] - pred_boxes[...,2]/2 
+        pre_top = pred_boxes[...,1] - pred_boxes[...,3]/2 
+
+        rect1 = plt.Rectangle([pre_left[0,0,5,5],pre_top[0,0,5,5]],pred_boxes[0,0,5,5,2],pred_boxes[0,0,5,5,3],color="r",fill=False)
+        rect2 = plt.Rectangle([pre_left[0,1,5,5],pre_top[0,1,5,5]],pred_boxes[0,1,5,5,2],pred_boxes[0,1,5,5,3],color="r",fill=False)
+        rect3 = plt.Rectangle([pre_left[0,2,5,5],pre_top[0,2,5,5]],pred_boxes[0,2,5,5,2],pred_boxes[0,2,5,5,3],color="r",fill=False)
+
+        ax.add_patch(rect1)
+        ax.add_patch(rect2)
+        ax.add_patch(rect3)
+
+        plt.show()
+
+
 
 #---------------------------------------------------------------#
 #    计算先验框和真实框之间的交并比iou
