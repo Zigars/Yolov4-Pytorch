16ba8d17ec3bd4b9e0a706a76a84f0bddfea9cf3
Zigars <1370170007@qq.com>
2021-03-30 13:42:09 +0800

    更新nms

--- utils/utils.py
+++ utils/utils.py
@@ -3,13 +3,16 @@
 @Author:Zigar
 @Date:2021/03/11 11:35:40
 '''
+from numpy.core.defchararray import encode
 import torch
 import torch.nn as nn
 import math
 import numpy as np
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-
+from torchvision.ops import nms
+from PIL import Image
+import cv2
 
 #---------------------------------------------------------------#
 #    解码输出检测框
@@ -163,13 +166,78 @@ class DecodeBox(nn.Module):
         plt.show()
 
 
+#---------------------------------------------------------------#
+#    给图像增加灰条，实现不失真的resize
+#    https://blog.csdn.net/qq_36767550/article/details/112499530
+#---------------------------------------------------------------#
+def letterbox_img(image, size):
+    """
+    @param:
+    -------
+    image:输入的图片
+    size：resize的大小
+    @Returns:
+    -------
+    new_image:增加灰条后的图片
+    """
+    iw, ih = image.size
+    w, h = size
+    scale = min(w/iw, h/ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
+
+    image = image.resize((nw, nh), Image.BICUBIC)
+    new_img = Image.new('RGB', size, (128, 128, 128))
+    new_img.paste(image, ((w - nw)//2, (h - nh)//2))
+    return new_img
+
+
+#---------------------------------------------------------------#
+#    修正使用letterbox_img的图片的框
+#    这段代码没有注释 无法理解
+#    具体就是把boxs也按照scale放大后，根据黑边移动相应的距离
+#---------------------------------------------------------------#
+def yolo_correct_boxes(top, left, bottom, right, input_shape, image_shape):
+    """
+    @param:
+    -------
+    top, left, bottom, right：letter_img图片检测结果的上下左右距离
+    input_shape：输入图片的尺寸，这里是resize后的图片（416，416）
+    image_shape：要输出的原始图片尺寸
+    @Returns:
+    -------
+    boxs：修正后的检测框集合
+    """
+    new_shape = image_shape * np.min(input_shape / image_shape)
+
+    offset = (input_shape - new_shape) / 2. 
+    scale = input_shape / new_shape
+
+    box_yx = np.concatenate(((top + bottom) / 2, (left + right) / 2), axis=-1) / input_shape
+    box_hw = np.concatenate((bottom - top, right - left), axis=-1) / input_shape
+
+    box_yx = (box_yx - offset) * scale
+    box_hw *= scale
+
+    box_mins = box_yx - (box_hw / 2.)
+    box_maxs = box_yx + (box_hw / 2.)
+
+    boxes = np.concatenate([
+        box_mins[:, 0:1],
+        box_mins[:, 1:2],
+        box_maxs[:, 0:1],
+        box_maxs[:, 1:2]
+    ], axis=-1)
+    boxes *= np.concatenate([image_shape, image_shape], axis=-1)
+    return boxes
+
 
 #---------------------------------------------------------------#
 #    计算先验框和真实框之间的交并比iou
 #    https://blog.csdn.net/laizi_laizi/article/details/103463802
 #    https://wstchhwp.blog.csdn.net/article/details/108450062
 #---------------------------------------------------------------#
-def box_iou(_box_a, _box_b):
+def box_iou(_box_a, _box_b, x1y1x2y2=False):
     """
     @param:
     -------
@@ -179,43 +247,64 @@ def box_iou(_box_a, _box_b):
     -------
     iou:tensor, shape=(batch, feat_w, feat_h, anchor_num, 1)
     """
-    # box_a, box_b左上角(x1, y1)和右下角坐标(x2, y2)
-    b1_x1, b1_y1 = _box_a[:, 0] - _box_a[:, 2] / 2, _box_a[:, 1] - _box_a[:, 3] / 2
-    b1_x2, b1_y2 = _box_a[:, 0] + _box_a[:, 2] / 2, _box_a[:, 1] + _box_a[:, 3] / 2
-    b2_x1, b2_y1 = _box_b[:, 0] - _box_b[:, 2] / 2, _box_b[:, 1] - _box_b[:, 3] / 2
-    b2_x2, b2_y2 = _box_b[:, 0] + _box_b[:, 2] / 2, _box_b[:, 1] + _box_b[:, 3] / 2
-    # 生成两个和所给框相同shape的全0数组
-    box_a = torch.zeros_like(_box_a)
-    box_b = torch.zeros_like(_box_b)
+    if not x1y1x2y2:
+        # box_a, box_b左上角(x1, y1)和右下角坐标(x2, y2)
+        b1_x1, b1_y1 = _box_a[:, 0] - _box_a[:, 2] / 2, _box_a[:, 1] - _box_a[:, 3] / 2
+        b1_x2, b1_y2 = _box_a[:, 0] + _box_a[:, 2] / 2, _box_a[:, 1] + _box_a[:, 3] / 2
+        b2_x1, b2_y1 = _box_b[:, 0] - _box_b[:, 2] / 2, _box_b[:, 1] - _box_b[:, 3] / 2
+        b2_x2, b2_y2 = _box_b[:, 0] + _box_b[:, 2] / 2, _box_b[:, 1] + _box_b[:, 3] / 2
+
+        # 生成两个和所给框相同shape的全0数组
+        box_a = torch.zeros_like(_box_a)
+        box_b = torch.zeros_like(_box_b)
     # 保存转换后的框坐标(x,y,w,h) -> (x1,y1,x2,y2)
-    box_a[:, 0], box_a[:, 1], box_a[:, 2], box_a[:, 3] = b1_x1, b1_y1, b1_x2, b1_y2
-    box_b[:, 0], box_b[:, 1], box_b[:, 2], box_b[:, 3] = b2_x1, b2_y1, b2_x2, b2_y2
-    A = box_a.size(0)
-    B = box_b.size(0)
-    # 计算交集坐标
-    max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
-                       box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
-    min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
-                       box_b[:, :2].unsqueeze(0).expand(A, B, 2))
-    # 计算交集面积
-    inter_hw = torch.clamp((max_xy - min_xy), min=0)
-    inter = inter_hw[:, :, 0] * inter_hw[:, :, 1]
-    # print(inter)
+        box_a[:, 0], box_a[:, 1], box_a[:, 2], box_a[:, 3] = b1_x1, b1_y1, b1_x2, b1_y2
+        box_b[:, 0], box_b[:, 1], box_b[:, 2], box_b[:, 3] = b2_x1, b2_y1, b2_x2, b2_y2
+        A = box_a.size(0)
+        B = box_b.size(0)
+        # 计算交集坐标
+        max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
+                           box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
+        min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
+                           box_b[:, :2].unsqueeze(0).expand(A, B, 2))
+        # 计算交集面积
+        inter_hw = torch.clamp((max_xy - min_xy), min=0)
+        inter = inter_hw[:, :, 0] * inter_hw[:, :, 1]
+        # print(inter)
 
-    # 计算先验框和真实框各自的面积
-    area_a = ((box_a[:, 2] - box_a[:, 0]) *
-              (box_a[:, 3] - box_a[:, 1])).unsqueeze(1).expand_as(inter)  # [A, B]
-    area_b = ((box_b[:, 2] - box_b[:, 0]) *
-              (box_b[:, 3] - box_b[:, 1])).unsqueeze(0).expand_as(inter)  # [A, B]
+        # 计算先验框和真实框各自的面积
+        area_a = ((box_a[:, 2] - box_a[:, 0]) *
+                  (box_a[:, 3] - box_a[:, 1])).unsqueeze(1).expand_as(inter)  # [A, B]
+        area_b = ((box_b[:, 2] - box_b[:, 0]) *
+                  (box_b[:, 3] - box_b[:, 1])).unsqueeze(0).expand_as(inter)  # [A, B]
 
-    # 计算并集面积
-    union = area_a + area_b - inter
-    # print(union)
+        # 计算并集面积
+        union = area_a + area_b - inter
+        # print(union)
+    
+        # 计算IOU
+        iou = inter / union  # [A, B]
+    else:
+        b1_x1, b1_y1, b1_x2, b1_y2 = _box_a[:, 0], _box_a[:, 1], _box_a[:, 2], _box_a[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = _box_b[:, 0], _box_b[:, 1], _box_b[:, 2], _box_b[:, 3]
 
-    # 计算IOU
-    IOU = inter / union  # [A, B]
+        # print(b1_x1, b1_y1, b1_x2, b1_y2)
+        # print(b2_x1, b2_y1, b2_x2, b2_y2)
 
-    return IOU
+        inter_rect_x1 = torch.max(b1_x1, b2_x1)
+        inter_rect_y1 = torch.max(b1_y1, b2_y1)
+        inter_rect_x2 = torch.min(b1_x2, b2_x2)
+        inter_rect_y2 = torch.min(b1_y2, b2_y2)
+    
+        inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
+                     torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+                 
+        b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+        b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+    
+        iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return iou
 
 # # test iou
 # _box_a = torch.from_numpy(np.array([[100, 50, 200, 100]])).float()
@@ -312,3 +401,102 @@ def clip_by_tensor(t, t_min, t_max):
 # print(clip_by_tensor(b,min,max))
 # # print(torch.clamp(b, min , max))
 # print(torch.Tensor.clip(b, min, max))
+
+
+#---------------------------------------------------------------#
+#    非极大值抑制nms
+#---------------------------------------------------------------#
+def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
+    """
+    @param:
+    -------
+    prediction：预测结果框
+    num_classes：类别的数量
+    conf_thres：置信度阈值
+    nms_thres：非极大值抑制阈值
+    @Returns:
+    output：经过nms的输出框
+    -------
+    """
+    #---------------------------------------------------------------#
+    #    xywh -> x1y1x2y2
+    #    prediction [batch_size, num_anchors, 85]
+    #---------------------------------------------------------------#
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+    box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
+    box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
+    box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
+    prediction[:, :, :4] = box_corner[:, :, :4]
+
+    out_put = [None for _ in range(len(prediction))]
+    for image_i, image_pred in enumerate(prediction):
+        #---------------------------------------------------------------#
+        #    获取种类预测部分的max
+        #    torch.max():返回输入tensor中所有元素的最大值
+        #    https://blog.csdn.net/Z_lbj/article/details/79766690
+        #    class_conf  [num_anchors, 1]  种类置信度
+        #    class_pred  [num_anchors, 1]  种类
+        #---------------------------------------------------------------#
+        class_conf, class_pred = torch.max(image_pred[:, 5:5 + num_classes], 1, keepdims=True)
+
+        # 利用置信度进行第一轮筛选
+        conf_mask = (image_pred[:, 4] * class_conf[:, 0] >= conf_thres).squeeze()
+
+        # 根据置信度进行预测结果的筛选
+        image_pred = image_pred[conf_mask]
+        class_conf = class_conf[conf_mask]
+        class_pred = class_pred[conf_mask]
+        if not image_pred.size(0):
+            continue
+        #---------------------------------------------------------------#
+        #    此时detections  [num_anchors, 7]
+        #    7 = [x1, y1, x2, y2, obj_conf, class_conf, class_pred]
+        #---------------------------------------------------------------#
+        detections = torch.cat((image_pred[:, :5], class_conf.float(), class_pred.float()), 1)
+
+        # 获得预测结果包含的所有种类
+        unique_labels = detections[:, -1].cpu().unique()
+
+        if prediction.is_cuda:
+            unique_labels = unique_labels.cuda()
+            detections = detections.cuda()
+
+        for c in unique_labels:
+            # 获取某一类得分筛选后全部的预测结果
+            detections_class = detections[detections[:, -1] == c]
+
+            #---------------------------------------------------------------#
+            #    实现nms操作
+            #---------------------------------------------------------------#
+            # 按照存在物体的置信度排序
+            _, conf_sort_index = torch.sort(detections_class[:, 4] * detections_class[:, 5], descending=True)
+            detections_class = detections_class[conf_sort_index]
+            # 进行非极大值抑制
+            max_detections = []
+            while detections_class.size(0):
+                # 去除这一类置信度最高的，一步一步往下判断，判断重合程度是否大于nms_thres, 如果是则去掉
+                max_detections.append(detections_class[0].unsqueeze(0))
+                if len(detections_class) == 1:
+                    break
+                ious = box_iou(max_detections[-1], detections_class[1:], x1y1x2y2=True)
+                detections_class = detections_class[1:][ious < nms_thres]
+            # 堆叠
+            max_detections = torch.cat(max_detections).data
+
+            # #---------------------------------------------------------------#
+            # #    使用torchvison自带的nms进行筛选框
+            # #---------------------------------------------------------------#
+            # keep = nms(
+            #     detections_class[:, :4],
+            #     detections_class[:, 4] * detections_class[:, 5],
+            #     nms_thres
+            # )
+            # max_detections = detections_class[keep]
+
+            # 将经过nms后的检测框加入输出
+            out_put[image_i] = max_detections if out_put[image_i] is None else torch.cat(
+                (out_put[image_i], max_detections))
+    return out_put
+
+
