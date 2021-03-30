b54b9071c97b91836902a653289db3d90cfd0f47
Zigars <1370170007@qq.com>
2021-03-30 23:16:03 +0800

    检测推理

--- detect.py
+++ detect.py
@@ -1,6 +1,22 @@
 '''
-TODO
 @Description:检测
 @Author:Zigar
 @Date:2021/03/11 11:44:19
 '''
+from PIL import Image
+import os
+from yolo import YOLO
+
+yolo = YOLO()
+
+while True:
+    img = input('Input image filename:')
+    try:
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = yolo.detect_image(image)
+
+        r_image.show()
